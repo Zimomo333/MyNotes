56272ee701f26eacdce1fa682f751ba6396a50a3
Zimomo <lzh120908717@gmail.com>
2020-10-26 23:09:54 +0800

    typeof 和 instanceof 区别

--- Javascript/算法/test.js
+++ Javascript/算法/test.js
@@ -1,13 +1,2 @@
-function insertSort(arr){
-    for(var i=1;i<arr.length;i++){
-        var inserted=arr[i];
-        for(var j=i-1;j>=0 && inserted<arr[j];j--)
-            arr[j+1]=arr[j]
-        arr[j+1]=inserted
-    }
-    return arr
-}
-
-var arr=[8,7,6,5,4,3]
-
-console.log(insertSort(arr))
+var arr = []
+console.log(Array.isArray(arr))
