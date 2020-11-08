656ae0bffb15e8987d799277d788123f2f70c5c8
Zimomo <lzh120908717@gmail.com>
2020-11-08 23:22:58 +0800

    手盟笔试

--- Javascript/demo/test.js
+++ Javascript/demo/test.js
@@ -1,1 +1,10 @@
-console.log([3, 2, 1].reduce(Math.pow))
+function reverseMatrix(sourceArr) {
+  var reversedArr = []
+  for (var i = 0; i < sourceArr[0].length; i++) {
+    reversedArr[n] = []
+    for (var j = 0; j < sourceArr.length; j++) {
+      reversedArr[j][i] = sourceArr[i][j]
+    }
+  }
+  return reversedArr
+}
